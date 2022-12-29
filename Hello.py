bd7ab7c28f9ce1a8bcd63073ceaf3b572e90dc41
Tesfaye <tesemet@gmail.com>
2022-12-29 10:50:08 +0300

    code for push is added by tesfaye

--- Hello.py
+++ Hello.py
@@ -1,1 +1,44 @@
 hi there
+tesem@Theotocos MINGW64 ~
+$ cd
+
+tesem@Theotocos MINGW64 ~
+$ cd desktop
+
+tesem@Theotocos MINGW64 ~/desktop
+$ git clone https://github.com/NewayKetsela/Group-5.git
+Cloning into 'Group-5'...
+remote: Enumerating objects: 19, done.
+remote: Counting objects: 100% (19/19), done.
+remote: Compressing objects: 100% (12/12), done.
+remote: Total 19 (delta 4), reused 17 (delta 2), pack-reused 0
+Receiving objects: 100% (19/19), done.
+Resolving deltas: 100% (4/4), done.
+
+tesem@Theotocos MINGW64 ~/desktop
+$ code .
+
+tesem@Theotocos MINGW64 ~/desktop
+$ cd Group-5
+
+tesem@Theotocos MINGW64 ~/desktop/Group-5 (main)
+$ git add new
+
+tesem@Theotocos MINGW64 ~/desktop/Group-5 (main)
+$ git commit -m"commited"
+[main 270309b] commited
+ 1 file changed, 2 insertions(+), 1 deletion(-)
+
+tesem@Theotocos MINGW64 ~/desktop/Group-5 (main)
+$ git push -u origin main
+Enumerating objects: 5, done.
+Counting objects: 100% (5/5), done.
+Delta compression using up to 12 threads
+Compressing objects: 100% (2/2), done.
+Writing objects: 100% (3/3), 287 bytes | 287.00 KiB/s, done.
+Total 3 (delta 1), reused 0 (delta 0), pack-reused 0
+remote: Resolving deltas: 100% (1/1), completed with 1 local object.
+To https://github.com/NewayKetsela/Group-5.git
+   4bb546d..270309b  main -> main
+branch 'main' set up to track 'origin/main'.
+
