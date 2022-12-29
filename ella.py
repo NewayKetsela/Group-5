d7b8a1d0aeb5d5776114aaabc13151ee472aaae4
Ella-Jr <elhanan027@gmail.com>
2022-12-29 16:40:24 +0300

    Add: ella.py file

--- ella.py
+++ ella.py
@@ -1,4 +1,17 @@
-def say_hello():
-    print("Helllo Wprld")
-    print (say_hello)
-      
+''' melti line 
+comments uses '''
+def sum():
+    a,b,c=   "Orainge","Bananan", "Cherry"
+    d=2 
+    e=2.1
+    height= 1.75
+    weight= 56
+    print(type(a),type(d),type(e))
+    print(len(a),len(b),len(c))
+    Name = 'ella'
+    Age = 22
+    GPA = 3.25567756
+    print("my name is {:s} , i am {:d} years old and my gpa is {:.1f}".format (Name, Age, GPA))
+    return True
+sum()
+#this is a comment !!
